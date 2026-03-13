9e9cf1153dd4cd2671e528213d3fce71f795040a
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-03-13 13:40:17 +0000

    setting up foundations for header component

--- head-wind/src/App.jsx
+++ head-wind/src/App.jsx
@@ -1,31 +1,35 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import headwindIcon from "./assets/headwind_icon.png";
 import './App.css'
 import { WeatherCard } from "./WeatherCard";
+import Header from './Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  // Header bar //
-  const navigation = () => {
-    return (
-      <div className = 'header'>
-        <div className='rectangle'>
-          <h1>Headwind</h1>
-        </div>
-      </div>
-    )
-  };
-
 
   return (
-    <div>
-      {navigation()}
-      {WeatherCard()}
-    </div>
+    <Header></Header>
   );
+
+  // const [count, setCount] = useState(0)
+
+  // // Header bar //
+  // const navigation = () => {
+  //   return (
+  //     <div className='header'>
+  //       <div className='rectangle'>
+  //         <h1>Headwind</h1>
+  //       </div>
+  //     </div>
+  //   )
+  // };
+  //
+  //
+  // return (
+  //   <div>
+  //     {navigation()}
+  //     {WeatherCard()}
+  //   </div>
+  // );
 }
 
 export default App
