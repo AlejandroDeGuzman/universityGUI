5b28f32b919d99ab8726c9ede43dcb15ced2871c
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-03-18 16:18:56 +0000

    fixed adam branch -> fixed conflicts

--- head-wind/src/App.jsx
+++ head-wind/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import headwindIcon from "./assets/headwind_icon.png";
 import './App.css'
-import { WeatherCard } from "./WeatherCard";
-import ForecastCard from './ForecastCard';
+// import { WeatherCard } from "./WeatherCard";
+import ForecastCard from './components/forecastCard/ForecastCard';
 import Header from './components/headerNavBar/Header';
 
 function App() {
-  const [count, setCount] = useState(0) 
+  const [count, setCount] = useState(0)
 
   return (
     <div>
