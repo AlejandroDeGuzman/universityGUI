405fcae3d975559cd9696c2d586fb33019f8158b
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-03-18 15:44:36 +0000

    updated navbar -> no functionality yet

--- head-wind/src/components/headerNavBar/Header.jsx
+++ head-wind/src/components/headerNavBar/Header.jsx
@@ -1,25 +1,19 @@
 import "./Header.css";
+import logo from "../../assets/headwind_icon.png";
+import hamburger from "../../assets/hamburger.png";
+
 
 function Header() {
   return (
     <header>
       <nav className="navbar">
         <div className="nav-container">
-          <ul className="nav-links">
-            <li>
-              <a href="">Home</a>
-            </li>
-
-            <li>
-              <a href="">Alternative Routes</a>
-            </li>
-
-            <li>
-              <a href="">About</a>
-            </li>
-          </ul>
+          <div className="hamburger">
+            <img src={hamburger} alt="hamburger icon"></img>
+          </div>
 
           <div className="logo">
+            <img src={logo} alt="logo"></img>
             <h2>HeadWind</h2>
           </div>
         </div>
