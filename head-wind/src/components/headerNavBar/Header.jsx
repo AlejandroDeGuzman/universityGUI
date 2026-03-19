0329d8b5a203fb8f589c358193e0cdbd4ae9592d
Alejandro De Guzman <alejandrodeguzman@proton.me>
2026-03-19 16:06:27 +0000

    added basic hamburger menu

--- head-wind/src/components/headerNavBar/Header.jsx
+++ head-wind/src/components/headerNavBar/Header.jsx
@@ -1,26 +1,42 @@
+import { useState } from "react";
 import "./Header.css";
 import logo from "../../assets/headwind_icon.png";
 import hamburger from "../../assets/hamburger.png";
 
-
 function Header() {
+  const [open, setOpen] = useState(false);
+
   return (
     <header>
       <nav className="navbar">
         <div className="nav-container">
-          <div className="hamburger">
-            <img src={hamburger} alt="hamburger icon"></img>
+
+          <div
+            className="hamburger"
+            onClick={() => setOpen(!open)}
+          >
+            <img
+              src={hamburger}
+              alt="hamburger icon"
+            />
           </div>
 
           <div className="logo">
-            <img src={logo} alt="logo"></img>
+            <img src={logo} alt="logo" />
             <h2>HeadWind</h2>
           </div>
-        </div>
-      </nav>
 
-    </header >
+        </div>
+
+        <ul className={`nav-menu ${open ? "show" : ""}`}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">About</a></li>
+          <li><a href="/">Contact</a></li>
+        </ul>
+
+      </nav>
+    </header>
   );
 }
 
-export default Header
+export default Header;
